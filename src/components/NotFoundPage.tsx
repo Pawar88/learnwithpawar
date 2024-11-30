3aa07de4c676a3071342ebaabad2ef3fc839b6bb
Pawar88 <pawarravindra1989@gmail.com>
2024-11-30 08:56:09 +0530

    5th commit

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,8 +1,20 @@
 // pages/NotFoundPage.tsx
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage: React.FC = () => {
-  return <h1>404: Page Not Found</h1>;
+    return (
+        <section>
+            <div className="td_height_120 td_height_lg_80"></div>
+            <div className="td_height_120 td_height_lg_80"></div>
+          <div style={{ textAlign: "center", padding: "50px" }}>
+          <h1>404 - Page Not Found</h1>
+          <p>Sorry, the page you're looking for does not exist.</p>
+          <Link to={`/`}>Go back to Home</Link>
+        </div>
+        <div className="td_height_120 td_height_lg_80"></div>
+        </section>
+      );
 };
 
 export default NotFoundPage;
